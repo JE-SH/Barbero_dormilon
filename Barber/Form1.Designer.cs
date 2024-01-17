99910d1c7d322ce704c4d556023512818648f341
Jose Santana <73598536+JE-SH@users.noreply.github.com>
2024-01-16 19:35:09 -0600

    Updload project

---
+++ Barber/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Barber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonINICIO = new System.Windows.Forms.Button();
+            this.pictureBoxllegando = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxdurmiendo = new System.Windows.Forms.PictureBox();
+            this.pictureBoxcortando = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxsillas = new System.Windows.Forms.PictureBox();
+            this.pictureBoxbar = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxllegando)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxdurmiendo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxcortando)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxsillas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxbar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonINICIO
+            // 
+            this.buttonINICIO.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonINICIO.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonINICIO.ForeColor = System.Drawing.Color.Maroon;
+            this.buttonINICIO.Location = new System.Drawing.Point(941, 88);
+            this.buttonINICIO.Name = "buttonINICIO";
+            this.buttonINICIO.Size = new System.Drawing.Size(189, 85);
+            this.buttonINICIO.TabIndex = 28;
+            this.buttonINICIO.Text = "INICIAR";
+            this.buttonINICIO.UseVisualStyleBackColor = false;
+            this.buttonINICIO.Click += new System.EventHandler(this.buttonINICIO_Click);
+            // 
+            // pictureBoxllegando
+            // 
+            this.pictureBoxllegando.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxllegando.Image")));
+            this.pictureBoxllegando.Location = new System.Drawing.Point(-13, 374);
+            this.pictureBoxllegando.Name = "pictureBoxllegando";
+            this.pictureBoxllegando.Size = new System.Drawing.Size(205, 222);
+            this.pictureBoxllegando.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxllegando.TabIndex = 27;
+            this.pictureBoxllegando.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(95, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 357);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBoxdurmiendo
+            // 
+            this.pictureBoxdurmiendo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxdurmiendo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxdurmiendo.Image")));
+            this.pictureBoxdurmiendo.Location = new System.Drawing.Point(824, 273);
+            this.pictureBoxdurmiendo.Name = "pictureBoxdurmiendo";
+            this.pictureBoxdurmiendo.Size = new System.Drawing.Size(404, 323);
+            this.pictureBoxdurmiendo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxdurmiendo.TabIndex = 21;
+            this.pictureBoxdurmiendo.TabStop = false;
+            // 
+            // pictureBoxcortando
+            // 
+            this.pictureBoxcortando.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxcortando.Image")));
+            this.pictureBoxcortando.Location = new System.Drawing.Point(768, 278);
+            this.pictureBoxcortando.Name = "pictureBoxcortando";
+            this.pictureBoxcortando.Size = new System.Drawing.Size(519, 318);
+            this.pictureBoxcortando.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxcortando.TabIndex = 20;
+            this.pictureBoxcortando.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(231, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(134, 357);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(386, 28);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(114, 357);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 25;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(506, 28);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(130, 357);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 26;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBoxsillas
+            // 
+            this.pictureBoxsillas.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxsillas.Image")));
+            this.pictureBoxsillas.Location = new System.Drawing.Point(23, 63);
+            this.pictureBoxsillas.Name = "pictureBoxsillas";
+            this.pictureBoxsillas.Size = new System.Drawing.Size(688, 343);
+            this.pictureBoxsillas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxsillas.TabIndex = 19;
+            this.pictureBoxsillas.TabStop = false;
+            // 
+            // pictureBoxbar
+            // 
+            this.pictureBoxbar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxbar.Image")));
+            this.pictureBoxbar.Location = new System.Drawing.Point(730, 28);
+            this.pictureBoxbar.Name = "pictureBoxbar";
+            this.pictureBoxbar.Size = new System.Drawing.Size(205, 222);
+            this.pictureBoxbar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxbar.TabIndex = 29;
+            this.pictureBoxbar.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(941, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 85);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "DETENER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1275, 624);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxbar);
+            this.Controls.Add(this.buttonINICIO);
+            this.Controls.Add(this.pictureBoxllegando);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxdurmiendo);
+            this.Controls.Add(this.pictureBoxcortando);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBoxsillas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxllegando)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxdurmiendo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxcortando)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxsillas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxbar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonINICIO;
+        private System.Windows.Forms.PictureBox pictureBoxllegando;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxdurmiendo;
+        private System.Windows.Forms.PictureBox pictureBoxcortando;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBoxsillas;
+        private System.Windows.Forms.PictureBox pictureBoxbar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Barber/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+
+
+namespace Barber
+{
+    public partial class Form1 : Form
+    {
+        int clientes;
+        Queue<PictureBox> sillas;
+        int cortando;
+        bool entrando;
+        bool run;
+        Semaphore sem;
+
+        Random rand;
+        System.Timers.Timer myTimerSilla;
+        System.Timers.Timer myTimerCorte;
+        public Form1()
+        {
+            InitializeComponent();
+            sem = new Semaphore(1, 2);
+            sillas = new Queue<PictureBox>();
+            //sillas = 4;                         //semáforo de sillas
+            clientes = 0;                       //semáforo de clientes (no hay)
+            cortando = 0;                   //barbero no cortando aun
+            entrando = false;                          //no hay nadie
+            //rand = new Random();                //numero aleatorio 
+
+            reset();
+        }
+//----------------------------------------------------------------------------------------------------
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar.ToString()== "\u001b")
+            {
+                //this.Close();
+                MessageBox.Show("La barbería ha cerrado. Da clic al botón para abrir", "CERRANDO");
+                buttonINICIO.Enabled = true;
+                reset();
+            }
+
+        }
+        private void buttonINICIO_Click(object sender, EventArgs e)
+        {
+            run = true;
+            buttonINICIO.Enabled = false;
+            var seed = Environment.TickCount;
+            rand = new Random(seed);
+            myTimerSilla.Interval = rand.Next(0, 7000);
+            myTimerSilla.Enabled = true;
+            myTimerSilla.Elapsed += OnTimedEvent;
+            muestraClienteEspera();
+
+            hilo();
+        }
+        void hilo()
+        {
+            new Thread(barberia).Start();
+        }
+
+        //Se muestran los clientes según la cantidad de gente en las sillas
+        void muestraClienteEspera()
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            pictureBox4.Hide();
+            if (sillas.Count > 0)
+            {
+                foreach (PictureBox silla in sillas)
+                {
+                    silla.Show();
+                }
+            }
+            pictureBox1.Refresh();
+            pictureBox2.Refresh();
+            pictureBox3.Refresh();
+            pictureBox4.Refresh();
+        }
+        //Devuelve la una silla disponible
+        public PictureBox seekLastPicture()
+        {
+            if (pictureBox1.Visible == false)
+                return pictureBox1;
+            else if (pictureBox2.Visible == false)
+                return pictureBox2;
+            else if (pictureBox3.Visible == false)
+                return pictureBox3;
+            else if (pictureBox4.Visible == false)
+                return pictureBox4;
+            else
+                return null;
+        }
+        //Wait & signal
+        public void waitClientes()
+        {
+            clientes++;
+        }
+        public void signalClietes()
+        {
+            clientes = clientes - 1;
+        }
+        public void waitSillas(PictureBox pic)
+        {
+            sillas.Enqueue(pic);
+        }
+        public void waitSillas()
+        {
+            PictureBox pic = seekLastPicture();
+            if (pic != null)
+            {
+                sillas.Enqueue(pic);
+            }
+        }
+        public void signalSillas()
+        {
+            sillas.Dequeue();
+        }
+        public bool isBarberReady()
+        {
+            return Convert.ToBoolean(cortando);
+        }
+        public void changeBarberReady()
+        {
+            cortando = Convert.ToInt32(!Convert.ToBoolean(cortando));
+        }
+
+        //-----ENTRA NUEVO CLIENTE
+        void muestraEntrando()
+        {
+            var seed = Environment.TickCount;
+            rand = new Random(seed);
+
+            pictureBoxllegando.Show();
+            pictureBoxllegando.Refresh();
+            Thread.Sleep(2500);
+            pictureBoxllegando.Hide();
+            pictureBoxllegando.Refresh();
+
+            if (clientes == 0) //AUN NO HAY CLIENTES
+            {
+                //waitSillas();   no es necesaria ya que no se sienta en las sillas
+                waitClientes();
+                cortando=1;     //pasa a ser a "cortando"
+                //tiempo aleatorio para hacer el corte
+                myTimerCorte.Interval = rand.Next(2000, 15000);
+                myTimerCorte.Enabled = true;
+                myTimerCorte.Elapsed += OnTimedCut;
+            }
+            else if (clientes > 0)//YA HAY CLIENTES (cortando o esperando)
+            {
+                waitSillas();   //ingresa un cliente a las sillas
+                waitClientes(); //
+            }
+            
+            //tiempo aleatorio para un nuevo ingreso
+            myTimerSilla.Interval = rand.Next(1000, 7000);
+            myTimerSilla.Enabled = true;
+
+            entrando = false;
+            //muestraClienteEspera(sillas);
+            muestraCortando();
+        }
+        //---- ACCIONES DEL BARBERO
+        void MuestraDurmiendo()
+        {
+            pictureBoxdurmiendo.Show();
+            pictureBoxcortando.Hide();
+            pictureBoxcortando.Refresh();
+            pictureBoxdurmiendo.Refresh();
+
+            //muestraClienteEspera(0);
+        }
+        void muestraCortando()
+        {
+            pictureBoxdurmiendo.Hide();
+            pictureBoxcortando.Show();
+            pictureBoxcortando.Refresh();
+            pictureBoxdurmiendo.Refresh();
+        }
+
+        //*********************ON TIME EVENT*********************
+        private void OnTimedEvent(Object source, EventArgs e)
+        {
+            if (!entrando)
+            {
+                myTimerSilla.Stop();
+                entrando = true;
+            }
+        }
+        private void OnTimedCut(Object source, EventArgs e)
+        {
+            cortando = 0;
+            myTimerCorte.Stop();
+
+        }
+        //*********************BARBERÍA*********************
+        public void barberia()
+        {
+            sem.WaitOne();
+            while(run)
+            {
+                var seed = Environment.TickCount;
+                rand = new Random(seed);
+
+                pictureBoxsillas.Refresh();
+
+                if (entrando)
+                {
+                    muestraEntrando();
+                    muestraClienteEspera();
+                }
+
+                if (clientes > 0)
+                {
+                    if (cortando == 0) //DEJÓ DE CORTAR
+                    {
+                        if (sillas.Count == 0)//Solo había un cliente en corte
+                        {
+                            signalClietes();
+                            cortando = 0;
+                        }
+                        else
+                        {
+                            signalClietes();
+                            signalSillas();
+                            cortando = 1;
+                            myTimerCorte.Interval = rand.Next(5000, 15000);
+                            myTimerCorte.Enabled = true;
+                            cortando = 1;
+                            
+                            pictureBoxdurmiendo.Show();
+                            pictureBoxcortando.Hide();
+                            Thread.Sleep(300);
+                            pictureBoxdurmiendo.Hide();
+                            pictureBoxcortando.Show();
+                            pictureBoxcortando.Refresh();
+                            pictureBoxdurmiendo.Refresh();
+
+                            muestraClienteEspera();
+
+                        }
+                    }
+                }
+                else //no tiene clientes
+                {
+                    MuestraDurmiendo();
+                    Thread.Sleep(500);
+                }
+            }
+            sem.Release();
+        }
+        void reset()
+        {
+
+            //ocultar imagenes no necesarias
+            pictureBoxsillas.Show();
+            pictureBoxdurmiendo.Show();
+            pictureBoxllegando.Hide();
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            pictureBox4.Hide();
+            pictureBoxcortando.Hide();
+            //temporizadores para entrar a la barberia
+            myTimerSilla = new System.Timers.Timer();//temporizador
+            //temporizador para cortar el pelo
+            myTimerCorte = new System.Timers.Timer();//temporizador
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            run = false;
+            //this.Close();
+        }
+    }
+}
